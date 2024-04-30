7c13b61a22f851941f09a6474a8667d9a1b4750a
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-30 05:46:23 -0700

    Fixed syntax

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,19 +1,6 @@
-# Define MySQL commands
-MYSQL_CMD="mysql -uroot -p"
-
-# Define database and user information
-DB_NAME="hbnb_dev_db"
-USER_NAME="hbnb_dev"
-USER_PASSWORD="hbnb_dev_pwd"
-
-# Create database if it doesn't exist
-$MYSQL_CMD -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-
-# Create user if it doesn't exist
-$MYSQL_CMD -e "CREATE USER IF NOT EXISTS '$USER_NAME'@'localhost' IDENTIFIED BY '$USER_PASSWORD';"
-
-# Grant privileges to the user on the database
-$MYSQL_CMD -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$USER_NAME'@'localhost';"
-
-# Grant SELECT privilege on performance_schema
-$MYSQL_CMD -e "GRANT SELECT ON performance_schema.* TO '$USER_NAME'@'localhost';"
+-- Script that prepares a MySQL server for the project
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
