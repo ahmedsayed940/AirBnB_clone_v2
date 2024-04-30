619efe131d0dbfd86037a65b3a7cc9f505afc5ba
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-30 06:01:34 -0700

    Fixed the class

--- models/user.py
+++ models/user.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 import os
 from sqlalchemy import Column, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    email = Column(String(128), nullable=False
+            ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    password = Column(String(128), nullable=False
+            ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    first_name = Column(String(128), nullable=True
+            ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    last_name = Column(String(128), nullable=True
+            ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
