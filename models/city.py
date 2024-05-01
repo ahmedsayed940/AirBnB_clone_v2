7dbb4f24302e96de179cd0dc0af483e96e1ec024
ahmedsayed940 <ahmeds.space940@gmail.com>
2024-05-01 05:41:58 -0700

    update city and state.py

--- models/city.py
+++ models/city.py
@@ -5,5 +5,7 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    return [city for city in storage.all(City).values() if city.state_id == self.id]
--- models/state.py
+++ models/state.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
+class State(BaseModel, Base):
+    __tablename__ = 'states'
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete-orphan", back_populates="state")
+
